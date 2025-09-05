ef3fa8664c91f538ef287b53ad58a6bac488c16c
Don-topo <rubengarridovaldepenas@gmail.com>
2025-09-05 19:56:51 +0200

    Prepare for vulkan

--- Main.cpp
+++ Main.cpp
@@ -10,11 +10,12 @@ std::string title = "GameEngine";
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello" << std::endl;
+    
     std::unique_ptr<GameEngine> gameEngine = std::make_unique<GameEngine>();
 
     gameEngine->Initialization(width, height, title);
     gameEngine->Loop();
     gameEngine->Cleanup();
+
     return 0;
 }

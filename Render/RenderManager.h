f5c58be4f8b255cb8ebe7066cf05db36e90cf307
Don-topo <rubengarridovaldepenas@gmail.com>
2025-09-06 17:37:48 +0200

    Add vma and stb dependencies

---
+++ Render/RenderManager.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <VkBootstrap.h>
+#include "vulkan/vulkan.h"
+#include "vk_mem_alloc.h"
+
+class RenderManager
+{
+public:
+	RenderManager();
+	~RenderManager();
+
+private:
+	
+};

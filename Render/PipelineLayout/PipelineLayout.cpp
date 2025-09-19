b6f8a82adab139f13a0c6176b299a2f9215b191e
Don-topo <rubengarridovaldepenas@gmail.com>
2025-09-19 19:01:06 +0200

    Work on PipelineLayout

---
+++ Render/PipelineLayout/PipelineLayout.cpp
@@ -0,0 +1,25 @@
+#include "PipelineLayout.h"
+
+void PipelineLayout::Initialization(VkDevice device, VkDescriptorSetLayout descriptorLayouts[], VkPushConstantRange pushConstants[])
+{
+	VkPipelineLayoutCreateInfo pipelineLayoutInfo = {};
+	pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+	pipelineLayoutInfo.setLayoutCount = static_cast<uint32_t>(sizeof(descriptorLayouts));
+	pipelineLayoutInfo.pSetLayouts = descriptorLayouts;
+	pipelineLayoutInfo.pushConstantRangeCount = static_cast<uint32_t>(sizeof(pushConstants));
+	pipelineLayoutInfo.pPushConstantRanges = pushConstants;
+
+	DEV_ASSERT(vkCreatePipelineLayout(device, &pipelineLayoutInfo, nullptr, &pipelineLayout), "PipelineLayout", "Error creating the PipelineLayout!");
+	DEV_LOG(TE_INFO, "PipelineLayout", "PipelineLayout created!");
+}
+
+void PipelineLayout::Cleanup(VkDevice device)
+{
+	if (pipelineLayout != nullptr)
+	{
+		vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
+		DEV_LOG(TE_INFO, "PipelineLayout", "PipelineLayout deleted!");
+		return;
+	}
+	DEV_LOG(TE_WARNING, "PipelineLayout", "Pipeline already deleted!");
+}
---
+++ Render/PipelineLayout/PipelineLayout.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../../Logger/Logger.h"
+#include <vulkan/vulkan.h>
+
+class PipelineLayout
+{
+public:
+	void Initialization(VkDevice device, VkDescriptorSetLayout descriptorLayouts[], VkPushConstantRange pushConstants[]);
+	VkPipelineLayout GetPipelineLayout() { return pipelineLayout; }
+	void Cleanup(VkDevice device);
+
+private:
+	VkPipelineLayout pipelineLayout;
+};
